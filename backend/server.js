c8f2bb6b609d73c1f25c23a0d38e4366ce9dae23
Anirudh Bhargav <anirudhdb@gmail.com>
2025-09-17 16:02:57 +0530

    Final Interview Ready Version

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
   res.send("Hello from backend server");
 });
 
-// Only start the server if NOT in test mode
+
 if (process.env.NODE_ENV !== "test") {
   app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
